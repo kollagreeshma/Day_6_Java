bbde2c997613c2f1d100abb33d86e0acd1441a62
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-07 16:23:44 +0530

    Add files via upload

---
+++ stringarray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+ class stringarray{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String arr[]=new String[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.next();
+        }
+        for(int i=0;i<n;i++){
+        System.out.print(arr[i]+" ");
+    }
+}
+ }
